1321090739b640fd5fee0babc9f71d75b9c53be0
AARUSHI29-G <aarushigullia@gmail.com>
2026-03-13 14:56:56 +0530

    feature/UC1

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,5 @@
 public class BookMyStayApp {
+    public static void main(strings[] args){
+        System.out.println("Welcome to the Hotel Booking Management System\nSystem initialized successfully.")
+    }
 }
